d9bb0d821d4efa1bc696016f7bf43efbe6e5233d
Marcos Monteiro <mrcsmonteiro@gmail.com>
2025-09-02 15:38:25 +1000

    Add apigw code and update outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "cloudfront_domain_name" {
   description = "The domain name of the CloudFront distribution"
   value       = aws_cloudfront_distribution.s3_distribution.domain_name
 }
+
+output "api_invoke_url" {
+  value = "${aws_api_gateway_stage.prod_stage.invoke_url}/visitors"
+}
