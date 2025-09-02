6c381e31610183f15cce83cd016970dde69f3401
Marcos Monteiro <mrcsmonteiro@gmail.com>
2025-09-02 13:51:20 +1000

    Add DynamoDB table and items

---
+++ dynamodb.tf
@@ -0,0 +1,24 @@
+# DynamoDB Table to store the visitor count
+
+resource "aws_dynamodb_table" "visitor_counts" {
+  name         = "PageViews"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = local.common_tags
+}
+
+resource "aws_dynamodb_table_item" "global_count" {
+  table_name = aws_dynamodb_table.visitor_counts.name
+  hash_key   = aws_dynamodb_table.visitor_counts.hash_key
+
+  item = jsonencode({
+    id     = { S = "global_count" }
+    visits = { N = "0" }
+  })
+}
